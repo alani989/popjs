7ba06f30c555a0b1909f439baf0a9135b68c6e63
Michael Muckler  |  Seeking Opportunities <mikemucklerpromo@gmail.com>
2018-04-16 14:19:02 -0500

    Update index.js

---
+++ app/users/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+	login: (req, res) => {
+		 const { user } = req
+
+		 res.json(user)
+
+		//res.render('login')
+	},
+
+	logout: (req, res, next) => {
+		req.session.destroy((err) => {
+			if(err) return next(err)
+
+			req.logout()
+
+			res.sendStatus(200)
+		})
+	},
+
+	ping: function(req, res) {
+		res.sendStatus(200)
+	}
+}
